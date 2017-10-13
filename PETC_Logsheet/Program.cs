5fb5537b069fb86127a2fe977e1d4d44f6b19ee0
dadat <dvdbarredo@gmail.com>
2017-10-13 15:52:15 +0800

    Update for ver

--- PETC_Logsheet/Program.cs
+++ PETC_Logsheet/Program.cs
@@ -16,7 +16,7 @@ namespace PETC_Logsheet
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmLogIn());
+            Application.Run(new frmMainMenu());
         }
     }
 }
