b42a5197a70fd8cf08f64b515c1156de8cbcdee1
dadat <dvdbarredo@gmail.com>
2017-10-13 14:10:26 +0800

    Initial Commit Dat

---
+++ PETC_Logsheet/ClassTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PETC_Logsheet
+{
+    class ClassTest
+    {
+        public string MyTest { get; set; }
+
+        public string GetTest()
+        {
+            MyTest = "Testing";
+            return MyTest;
+        }
+    }
+}
