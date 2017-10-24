f8463d1da14dde629cd12f7a6c7439e07e80f908
dadat <dvdbarredo@gmail.com>
2017-10-24 11:25:18 +0800

    Stage commit dat test

---
+++ PETC_Logsheet/Dat/ClassTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PETC_Logsheet.Dat
+{
+    class ClassTest
+    {
+        public int myNum { get; set; }
+
+        public int thisNum()
+        {
+            return myNum;
+        }
+    }
+}
